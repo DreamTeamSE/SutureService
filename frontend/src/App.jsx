0df722ac23679bceae4e15002795433821d925b1
Max Martin <maxmartin54321@gmail.com>
2024-10-25 10:47:36 -0400

    added materialUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import StartSection from "./assets/components/Home/StartSection"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const theme = createTheme({
+  palette: {
+    mode: 'light', // Change to 'dark' for dark mode
+    primary: {
+      main: '#3B00B9', // Customize primary color
+    },
+    secondary: {
+      main: '#dc004e', // Customize secondary color
+    },
+    background: {
+      default: '#F2F8FF', // Background color for the app
+    },
+  },
+});
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <StartSection/>
+      </ThemeProvider>
     </>
   )
 }
---
+++ frontend/src/assets/components/Home/StartSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@mui/material';
+
+
+const StartSection = () => {
+  return (
+    <>
+    <Button variant="contained" color="primary">
+      Click Me
+    </Button>
+    </>
+  )
+}
+
+export default StartSection
