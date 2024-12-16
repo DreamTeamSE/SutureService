c60730c52e272ce35dabeaffe723f71f574d22a1
Max Martin <maxmartin54321@gmail.com>
2024-12-16 14:55:37 -0500

    Updated Dockerfile

--- backend/app/db/Database.py
+++ backend/app/db/Database.py
@@ -10,7 +10,7 @@ class Database:
 
     def __init__(self):
         if not hasattr(self, 'pool'):
-            self.pool = SimpleConnectionPool(1, 100000000, user="postgres", password="example", host="localhost", port=5432, database="suturedb")
+            self.pool = SimpleConnectionPool(1, 100000000, user="myuser", password="mypassword", host="mydatabase", port=5432, database="suturedb")
 
     def getConnection(self):
         return self.pool.getconn()
