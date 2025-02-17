90230ca8e5d4897610ecca38f097b144558194f3
Max Martin <maxmartin54321@gmail.com>
2025-02-17 13:40:40 -0500

    containerized services

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+docker build -t SutureService/arduino_manager/prod:latest ../arduino_manager/.
+docker build -t SutureService/backend/prod:latest ../backend/.
+docker build -t SutureService/frontend/prod:latest ../frontend/.
+
